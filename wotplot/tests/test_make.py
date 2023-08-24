25de27b13b0ca906265f82a166cb90e15054ab5f
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2023-08-23 20:34:55 -0700

    TST: make() with bad chars

--- wotplot/tests/test_make.py
+++ wotplot/tests/test_make.py
@@ -16,3 +16,21 @@ def test_make_simple():
             ]
         ),
     )
+
+
+def test_make_bad_chars():
+    # bad char in s2
+    with pytest.raises(ValueError) as e:
+        make("ACGTC", "ACDTC", 2)
+    assert "Input sequence contains character D" in str(e.value)
+
+    # bad char in s1
+    with pytest.raises(ValueError) as e:
+        make("ACXTC", "ACCTC", 2)
+    assert "Input sequence contains character X" in str(e.value)
+
+    # bad char in s1 and s2; s1 should have priority in the error message (but
+    # this doesn't really matter that much tbh)
+    with pytest.raises(ValueError) as e:
+        make("ACXTC", "ACDTC", 2)
+    assert "Input sequence contains character X" in str(e.value)
