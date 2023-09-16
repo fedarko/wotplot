c87ef7c02f53b01c79d577459479c0886dc34881
Marcus Fedarko <mfedarko@ucsd.edu>
2023-09-16 05:14:16 -0700

    PERF/MNT: be consistent with deleting SAs in make
    also better doc for first comment on this

--- wotplot/_make.py
+++ wotplot/_make.py
@@ -314,9 +314,10 @@ def _make(s1, s2, k, yorder="BT", binary=True, verbose=False):
         s1, s2, k, s1_sa, s2_sa, matches, yorder=yorder, binary=binary
     )
     _mlog(f"found {len(matches):,} forward match cell(s).")
-    # I'm not sure if this is needed, but we might as well be very clear that
-    # "hey this big chunky suffix array is now unnecessary please garbage
-    # collect it"
+    # I'm not sure if this makes a difference (is Python smart enough to
+    # immediately garbage-collect s2_sa at this point?), but we might as
+    # well be very clear that "hey this big chunky suffix array is now
+    # unnecessary please garbage collect it"
     del s2_sa
 
     _mlog("computing ReverseComplement(s2)...")
@@ -337,6 +338,7 @@ def _make(s1, s2, k, yorder="BT", binary=True, verbose=False):
         s2isrc=True,
     )
     _mlog(f"found {len(matches):,} total match cell(s).")
+    del s1_sa
     del rcs2_sa
     density = 100 * (len(matches) / (mat_shape[0] * mat_shape[1]))
     _mlog(f"density = {density:.2f}%.")
