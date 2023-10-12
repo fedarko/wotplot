6c126b22fe1c5ea56df65bd0a99f077f6d44b313
Marcus Fedarko <mfedarko@ucsd.edu>
2023-10-11 17:38:14 -0700

    MNT/TST: abstract & test scipy version checking
    Much more thorough than before, I think.
    
    I think the get_sm_constructor() code should also be completely tested
    now, considering the full spectrum of python versions we're testing
    against...? that's cool!

--- wotplot/_scipy_sm_constructor_getter.py
+++ wotplot/_scipy_sm_constructor_getter.py
@@ -1,11 +1,24 @@
 import scipy
 
 
-def bail(sv):
+def bail(v):
     raise RuntimeError(
-        f'Hey, the SciPy version installed is "{sv}", and I don\'t know '
+        f'Hey, the SciPy version installed is "{v}", and I don\'t know '
         "how to parse that. Please open a GitHub issue -- sorry!"
     )
+
+
+def get_vnums(v):
+    vnum_parts = v.split(".")
+    if len(vnum_parts) >= 2:
+        try:
+            major_num = int(vnum_parts[0])
+            minor_num = int(vnum_parts[1])
+        except ValueError:
+            bail(v)
+        return (major_num, minor_num)
+    else:
+        bail(v)
 
 
 def get_sm_constructor():
@@ -15,22 +28,12 @@ def get_sm_constructor():
     break eventually, but for now it works and allows us to support Python
     3.6 through 3.11.
     """
-    sv = scipy.__version__
-    vnum_parts = sv.split(".")
+    major_num, minor_num = get_vnums(scipy.__version__)
+    if major_num >= 2 or (major_num == 1 and minor_num >= 8):
+        from scipy.sparse import coo_array
 
-    if len(vnum_parts) >= 2:
-        try:
-            major_num = int(vnum_parts[0])
-            minor_num = int(vnum_parts[1])
-        except ValueError:
-            bail(sv)
-        if major_num >= 2 or (major_num == 1 and minor_num >= 8):
-            from scipy.sparse import coo_array
-
-            return coo_array
-        else:
-            from scipy.sparse import coo_matrix
-
-            return coo_matrix
+        return coo_array
     else:
-        bail(sv)
+        from scipy.sparse import coo_matrix
+
+        return coo_matrix
--- wotplot/tests/test_scipy_sm_constructor_getter.py
+++ wotplot/tests/test_scipy_sm_constructor_getter.py
@@ -1,8 +1,5 @@
-# The array and DotPlotMatrix imports are needed for the
-# test_eval_of_repr_equals_orig() test -- if these two terms haven't already
-# been loaded, eval() will throw an error.
 import pytest
-from wotplot._scipy_sm_constructor_getter import bail
+from wotplot._scipy_sm_constructor_getter import bail, get_vnums
 
 
 def test_bail():
@@ -12,3 +9,21 @@ def test_bail():
         'Hey, the SciPy version installed is "sus version", and I don\'t know '
         "how to parse that. Please open a GitHub issue -- sorry!"
     )
+
+
+def test_get_vnums_good():
+    assert get_vnums("1.2") == (1, 2)
+    assert get_vnums("2.3.1") == (2, 3)
+    assert get_vnums("1.50") == (1, 50)
+    assert get_vnums("1234.5678") == (1234, 5678)
+    assert get_vnums("100.35.291038123") == (100, 35)
+
+
+def test_get_vnums_bad():
+    for bv in ("a.b", "evil", "1 2 3", ""):
+        with pytest.raises(RuntimeError) as ei:
+            get_vnums(bv)
+        assert str(ei.value) == (
+            f'Hey, the SciPy version installed is "{bv}", and I don\'t know '
+            "how to parse that. Please open a GitHub issue -- sorry!"
+        )
