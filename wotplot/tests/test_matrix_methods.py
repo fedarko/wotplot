def1df439f3b4d5312f32ccbdb418b677facfcfd
Marcus Fedarko <mfedarko@ucsd.edu>
2024-12-28 23:19:46 -0500

    TST: fix matrix methods tests

--- wotplot/tests/test_matrix_methods.py
+++ wotplot/tests/test_matrix_methods.py
@@ -1,18 +1,20 @@
 from wotplot import DotPlotMatrix
 
 
-def test_matrix_str_BT_binary():
+def test_matrix_str_BT():
     d = DotPlotMatrix("ACC", "CCA", 2)
-    assert str(d) == "DotPlotMatrix(k = 2, binary, bottom \u2192 top): 2 x 2"
+    assert str(d) == "DotPlotMatrix(k = 2, bottom \u2192 top): 2 x 2"
 
 
-def test_matrix_str_TB_binary():
+def test_matrix_str_TB():
     d = DotPlotMatrix("ACC", "CCA", 2, yorder="TB")
-    assert str(d) == "DotPlotMatrix(k = 2, binary, top \u2192 bottom): 2 x 2"
+    assert str(d) == "DotPlotMatrix(k = 2, top \u2192 bottom): 2 x 2"
 
 
-def test_matrix_str_TB_not_binary():
-    d = DotPlotMatrix("ACC", "CCA", 2, yorder="TB", binary=False)
+def test_matrix_str_TB_suff_only():
+    # I mean this SHOULDN'T impact the string representation of this matrix but
+    # who knows may as well be paranoid
+    d = DotPlotMatrix("ACC", "CCA", 2, yorder="TB", suff_only=True)
     assert str(d) == "DotPlotMatrix(k = 2, top \u2192 bottom): 2 x 2"
 
 
@@ -22,4 +24,4 @@ def test_matrix_repr():
     # object will vary depending on its type -- this would make this test super
     # brittle, and although I could account for this I don't think it's worth
     # the trouble
-    assert 'k=2, yorder="BT", binary=True)' in repr(d)
+    assert 'k=2, yorder="BT")' in repr(d)
